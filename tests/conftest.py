sbknana/project-pombal

test: add 26 tests for GEPA episode history check + fix conftest import
26 pytest tests covering:
- compute_keyword_overlap: 9 tests (identical, different, partial, empty, None, case, punctuation)
- extract_mutation_diff: 5 tests (added lines, removed lines, identical, empty, multiline)
- get_failed_episodes_by_keywords: 3 tests (filter by role, filter by outcome/q_value, empty DB)
- check_episode_history_for_candidate: 7 tests (penalize matching, no penalty novel, empty episodes,
  empty mutation, None mutation, multiple matches, custom threshold/penalty)
- Integration: 1 test (full structure validation)

Also fix conftest.py pytest_configure to handle ImportError gracefully when
equipa.cli is not available (e.g. in worktrees during monolith split).

tests/conftest.py
@@ -81,14 +81,18 @@ def _ensure_full_schema():
 
 def pytest_configure(config):
     """Ensure database schema exists before any tests collect."""
-    import forge_orchestrator
+    try:
+        import forge_orchestrator
 
-    # Reset ensure_schema cache so it re-runs for test DB
-    forge_orchestrator._SCHEMA_ENSURED = False
-    forge_orchestrator.ensure_schema()
+        # Reset ensure_schema cache so it re-runs for test DB
+        forge_orchestrator._SCHEMA_ENSURED = False
+        forge_orchestrator.ensure_schema()
 
-    # Apply the full schema.sql for tables not covered by ensure_schema()
-    _ensure_full_schema()
+        # Apply the full schema.sql for tables not covered by ensure_schema()
+        _ensure_full_schema()
+    except (ImportError, ModuleNotFoundError) as e:
+        print(f"  [conftest] WARNING: could not import forge_orchestrator: {e}")
+        print("  [conftest] Schema setup skipped — tests requiring DB may fail.")
 
 
 def pytest_collection_modifyitems(session, config, items):

sbknana/equipa

tests/conftest.py
@@ -81,14 +81,18 @@ def _ensure_full_schema():
 
 def pytest_configure(config):
     """Ensure database schema exists before any tests collect."""
-    from equipa import db as equipa_db
+    try:
+        from equipa import db as equipa_db
 
-    # Reset ensure_schema cache so it re-runs for test DB
-    equipa_db._SCHEMA_ENSURED = False
-    equipa_db.ensure_schema()
+        # Reset ensure_schema cache so it re-runs for test DB
+        equipa_db._SCHEMA_ENSURED = False
+        equipa_db.ensure_schema()
 
-    # Apply the full schema.sql for tables not covered by ensure_schema()
-    _ensure_full_schema()
+        # Apply the full schema.sql for tables not covered by ensure_schema()
+        _ensure_full_schema()
+    except (ImportError, ModuleNotFoundError) as e:
+        print(f"  [conftest] WARNING: could not import equipa.db: {e}")
+        print("  [conftest] Schema setup skipped — tests requiring DB may fail.")
 
 
 def pytest_collection_modifyitems(session, config, items):